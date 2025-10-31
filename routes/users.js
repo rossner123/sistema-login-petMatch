4207a2547432d7f8e38c3feeff2ca2e2024b340b
Bruno <brumassotti@gmail.com>
2025-10-31 08:41:50 -0300

    fix: arruma envio de arquivos

--- routes/users.js
+++ routes/users.js
@@ -2,14 +2,24 @@ import express from "express"
 import pool from "../src/db.js"
 import bcrypt from "bcrypt"
 import multer from "multer"
+import path from 'path'
+import { fileURLToPath } from "url"
 import { Router } from "express"
 const router = Router()
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename)
 
 router.use('/uploads', express.static('uploads'))
 
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => cb(null, 'uploads/'),
-  filename: (req, file, cb) => cb(null, Date.now())
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, "../uploads"))
+  },
+  filename: (req, file, cb) => {
+    const uniqueName = Date.now() + "-" + file.originalname;
+    cb(null, uniqueName)
+  }
 })
 
 const upload = multer({ storage })
@@ -72,13 +82,13 @@ router.post("/login", (req, res) => {
     res.status(201).json(result.rows[0])
   })
 
-  router.post("/:id/profile-image", upload.single('arquivo'), (req, res) => {
+  router.post("/:id/profile-image", upload.single('arquivo'), async (req, res) => {
     try {
       const userId = req.params.id;
       const { filename } = req.file
 
       const sql = 'UPDATE users SET profile_pic = $1 WHERE id = $2 RETURNING *'
-      const result = pool.query(sql, [filename, userId])
+      const result = await pool.query(sql, [filename, userId])
 
       res.json(result.rows)
     } catch(err) {
